ff0dde62f25471a0de54f31ab0415ae232c723b4
Kartiko Pramudito <kartikopramudito99@gmail.com>
2019-10-02 16:41:15 +0700

    Tampilan register dan login jadi

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -6,7 +6,17 @@ class Auth extends CI_Controller
 
     public function index()
     {
+        $this->load->view('templates/auth_header');
         $this->load->view('auth/login');
+        $this->load->view('templates/auth_footer');
+    }
+
+    public function registration()
+    {
+        $this->load->view('templates/auth_header');
+        $this->load->view('auth/registration');
+        $this->load->view('templates/auth_footer');
+
     }
 
 }
